ef90b477738270eb21b4450053ed912935eaac9a
Jonathan Suarez <jon.suarez92@gmail.com>
2022-11-05 20:42:48 -0400

    navbar setup

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">DJ_Lance</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Gallery</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            {/* <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">DJ_Lance</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Gallery</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar> */}
+
+
+        </div>
+
     )
 }
 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from '../Home/Home'
+//components
+import NavBar from '../../components/NavBar/NavBar';
 
 function App() {
   return (
     <main className="App">
       <>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
 
